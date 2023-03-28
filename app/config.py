b4dbf156dfad4988502dd34b0dd637645dc50dbd
Oussama Moutafatin <moutafatin3@gmail.com>
2023-03-28 17:36:18 +0000

    postgres env variables config

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings, PostgresDsn
+
+
+class Settings(BaseSettings):
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URL: PostgresDsn | None = None
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()  # type: ignore
